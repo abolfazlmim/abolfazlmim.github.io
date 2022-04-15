a314d6bd5751e3137408031cf8a0bd0316f18ce0
abolfazlmim <afmmyam@gmail.com>
2022-04-15 08:57:18 +0200

    💡JavaScript Tip: Did you know about the console.table() method?
    💡JavaScript Tip: Did you know about the console.table() method?

---
+++ jstips/script.js
@@ -0,0 +1,10 @@
+const people = [{
+    firtname: 'George',
+    hobby: 'playing football'
+}, {
+    firtname: 'John',
+    hobby: 'playing basketball'
+}]
+console.table(people)
+console.table(people , ['hooby'])
+console.table(people , ['firtname'])
